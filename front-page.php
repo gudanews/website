981efac680cdf5b667f50fbc4e2bb8cecc72d718
9999cn <houjiahua@hotmail.com>
2020-07-30 01:28:54 -0700

    Update MySQL database name

--- front-page.php
+++ front-page.php
@@ -1,8 +1,8 @@
 <?php get_header(); ?>
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
 
 // header('Location: index.php');
 $MAX_RECORD = 60;
