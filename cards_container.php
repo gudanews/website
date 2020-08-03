9a3ec98ca7545457c273ba1c67dc97843beb0000
9999cn <houjiahua@hotmail.com>
2020-08-02 23:52:54 -0700

    Add new pages

--- cards_container.php
+++ cards_container.php
@@ -1,5 +1,5 @@
 <?php
-$MAX_RECORD = 10;
+$MAX_RECORD = 20;
 $servername = "192.168.1.49";
 $username = "gudaman";
 $password = "GudaN3w2";
@@ -11,7 +11,6 @@ $heading = array();
 $snippet = array();
 $image_path = array();
 $url = array();
-$body = array();
 $source = array();
 $row_count = 0;
 
@@ -22,40 +21,38 @@ if ($conn->connect_error) {
 }
 
 $sql = "SELECT id, snippet FROM headline WHERE image_id > 0 AND datetime > '" . $day_minus_2d . "' ORDER BY datetime DESC LIMIT " . $MAX_RECORD;
-$result = $conn->query($sql);
-if ($result->num_rows > 0) {
-    while($row = $result->fetch_assoc()) {
-        $headline_id = $row["id"];
-        #$sql = "SELECT heading, body, news.url as url, image.url as image, source.name as source FROM news INNER JOIN image ON image_id = image.id INNER JOIN source ON source_id = source.id WHERE image.id > 0 LIMIT 3";
-        $sql = "SELECT heading, body, news.url as url, image.url as image, source.name as source FROM news INNER JOIN image ON image_id = image.id INNER JOIN source ON source_id = source.id WHERE headline_id = " . $headline_id;
-        $res = $conn->query($sql);
-        if ($res->num_rows > 0) {
+$result_headline = $conn->query($sql);
+if ($result_headline->num_rows > 0) {
+    while($row_headline = $result_headline->fetch_assoc()) {
+        $headline_id = $row_headline["id"];
+        $sql = "SELECT heading, snippet, news.url as url, image.url as image, source.name as source FROM news INNER JOIN image ON image_id = image.id INNER JOIN source ON source_id = source.id WHERE image.id > 0 ORDER BY RAND() LIMIT 3";
+        #$sql = "SELECT heading, news.url as url, snippet, image.url as image, source.name as source FROM news INNER JOIN image ON image_id = image.id INNER JOIN source ON source_id = source.id WHERE headline_id = " . $headline_id;
+        $result = $conn->query($sql);
+        if ($result->num_rows > 0) {
             $image_path[] = array();
             $heading[] = array();
             $snippet[] = array();
-            $body[] = array();
             $source[] = array();
             $url[] = array();
-            while($r = $res->fetch_assoc()) {
-                $image_path[$row_count][] = $r["image"];
-                $heading[$row_count][] = $r["heading"];
+            while($row = $result->fetch_assoc()) {
+                $image_path[$row_count][] = $row["image"];
+                $heading[$row_count][] = $row["heading"];
                 $snippet[$row_count][] = $row["snippet"];
-                $body[$row_count][] = $r["body"];
-                $source[$row_count][] = $r["source"];
-                $url[$row_count][] = $r["url"];
+                $source[$row_count][] = $row["source"];
+                $url[$row_count][] = $row["url"];
             }
             $row_count += 1;
         }
     }
 }
 $conn->close();
-######### END OF DB Connection
 ?>
+
 
 
 <div class="cards_container">
 <?php
-    for ($i=0; $i < $row_count; $i++) {
+    for ($i=0; $i < count($image_path); $i++) {
       if (!empty($image_path[$i])) {
         require_once "single_card_with_image.php";
         build_single_card_with_image($image_path[$i], $url[$i], $heading[$i], $source[$i]);
--- single_card_with_image.php
+++ single_card_with_image.php
@@ -15,92 +15,85 @@ function build_single_card_with_image($image, $url, $heading, $source) {
 $rand = getName(10);
 
 echo <<<EOL
-
-
-<div class = "homepage_card_header">
-      <ul class="nav nav-pills card-header-pills">
-
+<div horizontal layout class="image-card" style="width: 100%; height: 100%;">
+    <div class="image-card-media" style="display: inline-block; width: 38%; height: 38%;">
 EOL;
 
-for ($i = 0; $i < count($url); $i++) {
-    echo "<li class='nav-item'><a class='nav-link demo". $rand ."' onclick='currentCard" . $rand . "(" . $i;
-    echo ")'>" . $source[$i] . "</a></li>";
+for ($i = 0; $i < count($image); $i++) {
+echo <<<EOL
+        <div class="image$rand">
+            <a href="$url[$i]">
+                <img src="$image[$i]" style="width:100%; height=100%"/>
+            </a>
+        </div>
+EOL;
 }
 
 echo <<<EOL
-      </ul>
-      <div class = "homepage_sources_button_container_header">
-            <div class = "homepage_content_sources_bar_header">
-      </div>
-      </div>
-</div>
-
-<div class = "homepage_card_body">
-      <div class = "homepage_content_image">
+    </div>
+    <div class="image-card-info" style="display: inline-block; width: 60%; height: 60%;" vertical layout>
+    <div class="image-card-heading">
 EOL;
 
-for ($i = 0; $i < count($url); $i++) {
-    echo "<div class='myCards" . $rand . "'>";
-    echo "<a href='" . $url[$i] . "'>"
-        . "<img width='280' height='180' style='max-width: 100%; max-height: 100%'"
-        . " src='" . $image[$i] ."' /></a>";
-    echo "</div>";
+for ($i = 0; $i < count($heading); $i++) {
+echo <<<EOL
+            <div class="heading$rand">
+                <a href="$url[$i]">
+                    $heading[$i]
+                </a>
+            </div>
+EOL;
 }
 
 echo <<<EOL
-</div>
-<div class = "homepage_content_text">
-
-      <div class = "homepage_content_heading">
+        </div>
+        <div class="image-card-control" horizontal layout vertical-align="bottom">
+            <div class="image-card-meta">
+            </div>
+            <div class="iamge-card-nav">
+                <ul class="nav nav-pills card-header-pills">
 EOL;
-
-for ($i = 0; $i < count($url); $i++) {
-  echo "<div class='myCards" . $rand . "'>";
- 
-  echo "</div>";
+for ($i = 0; $i < count($source); $i++) {
+echo <<<EOL
+                    <li class="nav-item">
+                        <a class="nav-link source$rand" onclick="currentCard$rand($i)">
+                            $source[$i]
+                        </a>
+                    </li>
+EOL;
 }
 
 echo <<<EOL
-      </div>
-      <div class = "homepage_content_snippet">
+                </ul>
+            </div>
+        </div>
+    </div>
 
-      </div>
 </div>
-<div class = "homepage_buttons_content_container_for_features">
-      <!--add buttons here-->
-</div>
-</div>
-
 
 
 <script>
-        var cardIndex = 1;
-        showCards$rand(cardIndex);
+    var cardIndex = 0;
+    showCards$rand(cardIndex);
 
-        function currentCard$rand(n) {
-              showCards$rand(cardIndex = n);
+    function currentCard$rand(n) {
+        showCards$rand(cardIndex = n);
+    }
+
+    function showCards$rand(n) {
+        var images = document.getElementsByClassName("image$rand");
+        var headings = document.getElementsByClassName("heading$rand");
+        var sources = document.getElementsByClassName("source$rand");
+        for (i = 0; i < images.length; i++) {
+            images[i].style.display = "none";
+            headings[i].style.display = "none";
+            sources[i].className = sources[i].className.replace(" active", "");
         }
-
-        function showCards$rand(n) {
-              var i;
-              var cards = document.getElementsByClassName("myCards$rand");
-              var dots = document.getElementsByClassName("demo$rand");
-              if (n > cards.length) {cardIndex = 1}
-              if (n < 1) {cardIndex = cards.length}
-              for (i = 0; i < cards.length; i++) {
-                          cards[i].style.display = "none";
-                    }
-              for (i = 0; i < dots.length; i++) {
-                          dots[i].className = dots[i].className.replace(" active", "");
-
-
-              }
-              cards[cardIndex-1].style.display = "block";
-              dots[cardIndex-1].className += " active";
-
-        }
+        images[n].style.display = "block";
+        headings[n].style.display = "block";
+        sources[n].className += " active";
+    }
 </script>
-
 
 EOL;
 }
