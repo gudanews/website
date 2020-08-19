6305148c1240ee00a946fdb9fa153359981a25b6
9999cn <houjiahua@hotmail.com>
2020-08-19 03:03:33 -0700

    Update

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php
-// include "header.php";
 // ini_set('display_errors', 1);
 // ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+// error_reporting(E_ALL);
+
+
+include "header.php";
 
 function string_crop($string, $length) {
     $string = strip_tags($string);
@@ -19,7 +21,7 @@ function string_crop($string, $length) {
 
 function translate_to_chinese($text) {
     ob_start();
-    passthru("wp-content/themes/gudanews/translate.py '" . $text . "'");
+    passthru("script/translate.py '" . $text . "'");
     return ob_get_clean();
 }
 $q = $_GET['q'];
