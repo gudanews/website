2f270858ecad82ece9898e5eed78b75e0b605fef
gudaboy <eric.x.hou@gmail.com>
2020-08-20 01:07:29 -0700

    Update

---
+++ php/include.php
@@ -0,0 +1,25 @@
+<?php
+$SERVERNAME = "192.168.1.49";
+$USERNAME = "gudababy";
+$PASSWORD = "good";
+$DBNAME = "gudanews";
+
+function string_crop($string, $length) {
+    $string = strip_tags($string);
+    if (strlen($string) > $length) {
+        // truncate string
+        $stringCut = substr($string, 0, $length);
+        $endPoint = strrpos($stringCut, ' ');
+        //if the string doesn't contain any space then it will cut without word basis.
+        $string = $endPoint? substr($stringCut, 0, $endPoint) : substr($stringCut, 0);
+        $string .= ' ...';
+    }
+    return $string;
+}
+
+function translate_to_chinese($text) {
+    ob_start();
+    passthru("script/translate.py '" . $text . "'");
+    return ob_get_clean();
+}
+?>
