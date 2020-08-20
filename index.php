2a4bf715b09fe3214d7e3a06c26b71084f571a3c
gudaboy <eric.x.hou@gmail.com>
2020-08-20 00:52:30 -0700

    Update

--- index.php
+++ index.php
@@ -3,10 +3,9 @@
 // ini_set('display_startup_errors', 1);
 // error_reporting(E_ALL);
 
-include "php/user.php";
-
 include "header.php";
 
+include "php/user.php";
 
 $q = $_GET['q'];
 $lang = $_GET['lang'];
