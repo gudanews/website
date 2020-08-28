4e49e25bcb5ccb2b6c75bef7956e40587f8f6270
gudaman <houjiahua@hotmail.com>
2020-08-27 17:48:17 -0700

    update

--- container/card/pagination.php
+++ container/card/pagination.php
@@ -14,6 +14,7 @@ $day_minus_2w = date('Y-m-d', strtotime('-14 days'));
 
 $pageno = $_POST['pageno'] ?? 1;
 $lang = $_POST['lang'] ?? 0;
+$q = $_POST['q'] ?? '';
 $offset = ($pageno - 1) * $RECORD_PER_PAGE;
 
 $card_count = $offset;
@@ -23,21 +24,17 @@ $conn = new mysqli($SERVERNAME, $USERNAME, $PASSWORD, $DBNAME);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-$sql = <<<SQL
+if (empty($q)) {
+    $sql = <<<SQL
     SELECT id, datetime_created, datetime_updated FROM news WHERE datetime_created > '$day_minus_2d'
     AND is_indexed ORDER BY datetime_created DESC LIMIT $offset, $RECORD_PER_PAGE
-SQL;
-if (isset($q)) {
+    SQL;
+}
+else {
     $sql = <<<SQL
     SELECT id, datetime_created, datetime_updated FROM news WHERE datetime_created > '$day_minus_2w' AND
-    title LIKE '%$q%' OR snippet LIKE '%$q%' ORDER BY datetime_created DESC LIMIT $offset, $RECORD_PER_PAGE
-SQL;
-}
-elseif ($lang == 1) {
-    $sql = <<<SQL
-    SELECT id, datetime_created, datetime_updated FROM news WHERE datetime_created > '$day_minus_2d' AND
-    translation_id > 0 ORDER BY datetime_created DESC LIMIT $offset, $RECORD_PER_PAGE
-SQL;
+    is_indexed AND title LIKE '%$q%' OR snippet LIKE '%$q%' ORDER BY datetime_created DESC LIMIT $offset, $RECORD_PER_PAGE
+    SQL;
 }
 $result_cards = $conn->query($sql);
 
@@ -45,17 +42,17 @@ if ($result_cards->num_rows > 0) {
     while($row_cards = $result_cards->fetch_assoc()) {
         $cards_id = $row_cards['id'];
         $sql = <<<SQL
-    SELECT title, uuid, news.datetime_created as datetime_created, news.url as url, snippet, image.thumbnail as image,
-    source.short_name as source, color FROM news INNER JOIN image ON image_id = image.id INNER JOIN source 
-    ON source_id = source.id WHERE news.id = $cards_id
-SQL;
+        SELECT title, uuid, news.datetime_created as datetime_created, news.url as url, snippet, image.thumbnail as image,
+        source.short_name as source, color FROM news INNER JOIN image ON image_id = image.id INNER JOIN source 
+        ON source_id = source.id WHERE news.id = $cards_id
+        SQL;
         if ($lang == 1) {
             $sql = <<<SQL
-    SELECT translation.title as title, uuid, news.datetime_created as datetime_created, news.url as url,
-    translation.snippet as snippet, image.thumbnail as image, source.short_name as source, color 
-    FROM news INNER JOIN image ON image_id = image.id INNER JOIN source ON source_id = source.id
-    INNER JOIN translation ON translation_id = translation.id WHERE news.id = $cards_id
-SQL;
+            SELECT translation.title as title, uuid, news.datetime_created as datetime_created, news.url as url,
+            translation.snippet as snippet, image.thumbnail as image, source.short_name as source, color 
+            FROM news INNER JOIN image ON image_id = image.id INNER JOIN source ON source_id = source.id
+            INNER JOIN translation ON translation_id = translation.id WHERE news.id = $cards_id
+            SQL;
         }
         $result = $conn->query($sql);
         if ($result->num_rows > 0) {
