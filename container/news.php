7b9de6fc58feb27802d3e7f10d521bb6a9ccefd9
gudaman <houjiahua@hotmail.com>
2020-10-11 01:16:48 -0700

    update

--- container/news.php
+++ container/news.php
@@ -57,7 +57,7 @@ if(empty($author)){
     $author = "Unknown";
 }
 echo <<<EOL
-<title>$title - GudaNews</title>
+<title>$title - GUDANEWS.COM</title>
 <div class='news-card-container'>
     <div class='news-card-title'>
         <p class='news-title'>$title</p>
